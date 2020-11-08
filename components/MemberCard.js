d9bbbf6d76a9b0b27fb8be369612039990db49e9
Kavin Valli <kavinvalli@gmail.com>
2020-11-08 18:00:51 +0530

    Added CSS to member section and member card

--- components/MemberCard.js
+++ components/MemberCard.js
@@ -1,8 +1,9 @@
 import {Col} from 'react-bootstrap';
+import styles from './MemberCard.module.css';
 
 export default function MemberCard({ name, post }) {
 	return (
-			<Col xs={3} >
+			<Col xs={3} className={styles.memberCard}>
 			<div className="text">
 			<div className="name"><strong>{name}</strong></div>
 			<div className="post">{post}</div>
--- components/MemberSection.js
+++ components/MemberSection.js
@@ -1,10 +1,11 @@
 import { Col, Row } from "react-bootstrap";
 import MemberCard from "./MemberCard";
+import styles from './MemberSection.module.css';
 
 export default function MemberSection({ standard, students }) {
 	return (
-			<div>
-			<h2 style={{textAlign: "center"}}>Class {standard}</h2>
+			<div className={styles.section}>
+			<h2 style={{textAlign: "center", margin: "40px 0"}}>Class {standard}</h2>
 			<Row style={{justifyContent: "center"}}>
 			{students.map(member => (
 					<MemberCard name={member.name} post={member.post} />
