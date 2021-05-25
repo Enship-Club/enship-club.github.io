fb5cc4db215fa0790ff985ede997a4cdaf72d38c
Kavin Desi Valli <41034356+kavin25@users.noreply.github.com>
2021-05-25 17:07:52 +0530

    Update faculty.js

--- pages/faculty.js
+++ pages/faculty.js
@@ -1,15 +1,11 @@
 import { Container } from "react-bootstrap";
-import useSWR from 'swr';
+import faculty from "../data/faculty";
 import MemberSection from '../components/MemberSection';
 
 export default function Members() {
-  const fetcher = url => fetch(url).then(r => r.json())
-	const { data, error } = useSWR('/api/faculty', fetcher)
-	if (!data) return <div>Loading</div>
-		if (error) return <div>error</div>
 	return (
 		<Container>
-      {data.map((facultySection)=> (
+      {faculty.map((facultySection)=> (
 					<>
 					<MemberSection standard={facultySection.section} students={facultySection.faculties} />
 					</>
