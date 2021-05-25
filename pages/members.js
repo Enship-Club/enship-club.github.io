ab0130cb68a0202e79228cea6e6bbdde40d20bfe
Kavin Desi Valli <41034356+kavin25@users.noreply.github.com>
2021-05-25 17:03:58 +0530

    Get data from members.js and not api

--- pages/members.js
+++ pages/members.js
@@ -1,15 +1,11 @@
 import { Container } from "react-bootstrap";
-import useSWR from 'swr';
+import members from "../data/members"
 import MemberSection from '../components/MemberSection';
 
 export default function Members() {
-  const fetcher = url => fetch(url).then(r => r.json())
-	const { data, error } = useSWR('/api/members', fetcher)
-	if (!data) return <div>Loading</div>
-		if (error) return <div>error</div>
 	return (
 		<Container>
-      {data.map((studentClass)=> (
+      {members.map((studentClass)=> (
 					<>
 					<MemberSection standard={"Class " + studentClass.standard} students={studentClass.students} />
 					</>
