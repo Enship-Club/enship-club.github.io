9cd98784b5de14c3a7622f6aeb9bf4b51c2873c7
Kavin Desi Valli <41034356+kavin25@users.noreply.github.com>
2021-05-25 17:08:23 +0530

    Update alumni.js

--- pages/alumni.js
+++ pages/alumni.js
@@ -1,15 +1,11 @@
 import { Container } from "react-bootstrap";
-import useSWR from 'swr';
+import alumni from "../data/alumni";
 import MemberSection from '../components/MemberSection';
 
 export default function Members() {
-  const fetcher = url => fetch(url).then(r => r.json())
-	const { data, error } = useSWR('/api/alumni', fetcher)
-	if (!data) return <div>Loading</div>
-		if (error) return <div>error</div>
 	return (
 		<Container>
-      {data.map((alumniBatch)=> (
+      {alumni.map((alumniBatch)=> (
 					<>
 					<MemberSection standard={"Batch of " + alumniBatch.batch} students={alumniBatch.students} />
 					</>
