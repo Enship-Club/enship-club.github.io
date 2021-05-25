892d5248ba0b27c35613fda3a37e1d67c18b8287
Kavin Desi Valli <41034356+kavin25@users.noreply.github.com>
2021-05-25 17:09:16 +0530

    Update MainEvents.js

--- components/MainEvents.js
+++ components/MainEvents.js
@@ -1,18 +1,14 @@
 import { Row, Container, Col } from "react-bootstrap";
 import MainEventCard from "./MainEventCard";
-import useSWR from 'swr';
+import mainEvents from "../data/mainEvents";
 
 export default function MainEvents() {
-  const fetcher = url => fetch(url).then(r => r.json())
-	const { data, error } = useSWR('/api/mainEvents', fetcher)
-	if (!data) return <div>Loading</div>
-		if (error) return <div>error</div>
 	return (
 			<div style={{margin: "20px 15%"}}>
 			<Container>
 			<h3 style={{textAlign: "center"}}>Main Events</h3>
 			<Row>
-      {data.map((event)=> (
+      {mainEvents.map((event)=> (
 					<Col sm={12} md={6}>
 					<MainEventCard key={data.indexOf(event)} heading={event.heading} body={event.body} />
 					</Col>
