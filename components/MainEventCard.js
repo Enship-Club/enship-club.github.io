92023a9859d75b3291267fbda12d0e20bb44bd96
Samyak Kapoor <66072976+Samyak-code@users.noreply.github.com>
2021-07-27 18:52:49 +0530

    Update MainEventCard.js

--- components/MainEventCard.js
+++ components/MainEventCard.js
@@ -1,20 +1,24 @@
+import React from 'react';
 import { Card } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import handleShow from "/components/ModalEventCard";
 
-export default function MainEventCard({ heading, body }) {
+export default function MainEventCard({ heading, body, ModalHeading, Description }) {
   return (
-    <Card
-      style={{
-        padding: "5px",
-        marginBottom: "12px",
-        maxWidth: "490px",
-        height: "100%",
-      }}
-    >
-      <Card.Body style={{ textAlign: "center" }}>
-        <Card.Title>{heading}</Card.Title>
-        <hr />
-        <Card.Text>{body}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div style={{flexBasis:"0.7px",border:"1px solid #333",padding:"2.5rem", borderRadius: "10px"}} onClick={openModal}>
+      <h5><a target="_blank">{heading}</a></h5><div><p>{body}</p></div>
+    </div>
   );
-}
+  function openModal(){
+  render() ;{
+    <Modal backdrop="static">
+      <Modal.Header closeButton>
+        <Modal.Title>{ModalHeading}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{Description}</Modal.Body>
+      <Modal.Footer><button type="button" variant="secondary" class="btn btn-default">Close</button></Modal.Footer>
+    </Modal>
+  }
+}}
