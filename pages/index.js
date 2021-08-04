82bd827b8c7d616beec00f0fcc8653353f0b522c
Kavin valli <kavinvalli@gmail.com>
2021-08-04 08:35:09 +0530

    feat: Remove events section in home page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Banner from "../components/Banner";
-import StayTuned from "../components/StayTuned";
-import MainEvents from "../components/MainEvents";
-import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div style={{ backgroundColor: "#f1f1f1" }}>
       <Banner />
       {/* <StayTuned /> */}
-      <MainEvents />
+      {/** <MainEvents /> */}
+      <div>Add something here...</div>
     </div>
   );
 }
